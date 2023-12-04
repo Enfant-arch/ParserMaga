59a4dee1cc25ed931dc0c50a298a9174a6c98bcf
Enfant-Arch <frigmortuum@gmail.com>
2023-12-04 09:07:48 +0300

    forgot

---
+++ Bot/Program.cs
@@ -0,0 +1,42 @@
+﻿using Bot.Services;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+
+var botClient = new TelegramBotClient("5642967169:AAE5tWm8uXqpK2O8X4_4_7sQerNcSm4Ev_c");
+var cts = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = { } // receive all update types
+};
+
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken: cts.Token);
+
+Console.WriteLine($"Bot is up and running.");
+Console.ReadLine();
+
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    
+    if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+    {
+        var messageService = new AdminServices(botClient);
+        await messageService.HandleMessageAsync(update.Message);
+    }
+}
+
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    // Handle the error here
+    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    return Task.CompletedTask;
+}
---
+++ Parser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Npgsql;
+using NUnit.Framework;
+using Parser;
+using Parser.Parser;
+using Parser.Parser.Global;
+using Parser.Parser.GoodSorted;
+using Parser.utlis;
+
+class Program
+{
+
+    static async Task Main()
+    {
+       
+        var browserManager = new BrowserManager();
+        var parser = new ProductParser(browserManager);
+        database.ConnectString = "server=localhost;port=5432;database=postgres;username=postgres;password=root";
+        using var connection = new NpgsqlConnection(database.ConnectString);
+        await connection.OpenAsync();
+        GoodPlace.GlobalUrls = new List<string>();
+        GoodPlace.NameUrls = new Dictionary<string, string>();
+        await BrowserManager.generator_of_urls();
+        var parseTasks = GoodPlace.GlobalUrls.Select(url => parser.ParseProductAsync(url));
+       
+        await Task.WhenAll(parseTasks);
+        await browserManager.DisposePlaywrightAsync();
+
+        
+
+    }
+    
+}
